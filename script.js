e37dd755960e61ffe33cfd94a5896cd2285280ac
log10 <lokesh.noble@gmail.com>
2015-03-05 18:47:39 +0530

    50% progress

--- script.js
+++ script.js
@@ -1,22 +1,85 @@
 $(document).ready(function(){
+    var images=["barber","brahmin","guard","king","tenali","barbers","brahmins","guards","kings"];
+    var character=[];
+    var mapCharacter=[];
+    $.getJSON("StoryChar.json",function(json){
+        $.each(json,function(){
+            character.push(JSON.stringify(this));
+            mapCharacter.push("man1.png");
+            var subs=this.split(" ");
+            for(var i=0;i<subs.length;i++){
+
+                subs[i]=subs[i].toLowerCase();
+                console.log("subs thing "+subs[i]);
+            }
+            for(var i=0;i<subs.length;i++){
+                for(var j=0;j<images.length;j++){
+                    console.log(subs[i]==images[j]+" "+subs[i]+" == "+images[j]);
+                    if( subs[i]==images[j] ){
+                        mapCharacter[(mapCharacter.length-1)]=(images[j]+".png");
+                        break;
+                    }
+                    /*else if(i=(subs.length-1)){
+                        mapCharacter.push("man1.png");
+                        break;
+                    }*/
+                }
+            }
+        });
+        console.log(character);
+    console.log(mapCharacter);
+    });
+    //console.log(character);
+    //console.log(mapCharacter);
 
    $("#press").click(function(){
         //$.getJSON("content.json",function(json){
-        $.getJSON("content.json",function(json){
+        $.getJSON("Story.json",function(json){
             //var content="<div id='main' class='main'>";
             var bg="";
             var ch1="";
             var ch2="";
 		$.each(json,function(){
-                    var content='<div id="main" class="main" style="background-image:url(images/'+this['background']+')">';
+                    var dia1="";
+                    var dia2="";
+                    //var content='<div id="main" class="main" style="background-image:url(images/'+this['background']+')">';
+                    //console.log(this['dialogue'].length);
+                    if(this['dialogue'].length==1){
+                        dia1=this['dialogue'][0];
+                    }
+                    else if(this['dialogue'].length==2){
+                        dia1=this['dialogue'][0];
+                        dia2=this['dialogue'][1];
+                    }
+                    alert("hello");
+                    var char1="";
+                    var char2="";
+                    if(this['character'].length==1){
+                        console.log("index of character "+character.indexOf('"'+this['character'][0]+'"'));
+
+                        var index=parseInt(character.indexOf('"'+this['character'][0]+'"'));
+                        char1=mapCharacter[character.indexOf('"'+this['character'][0]+'"')];
+                        console.log("corres image "+mapCharacter[character.indexOf(this['character'][0])]);
+                    }
+                    if(this['character'].length>1){
+                        console.log("index of character "+character.indexOf('"'+this['character'][0]+'"'));
+
+                        var index1=parseInt(character.indexOf('"'+this['character'][0]+'"'));
+                        var index2=parseInt(character.indexOf('"'+this['character'][1]+'"'));
+                        char1=mapCharacter[character.indexOf('"'+this['character'][0]+'"')];
+                        char2=mapCharacter[character.indexOf('"'+this['character'][1]+'"')];
+                        console.log(character);
+                        console.log("corres image "+mapCharacter[character.indexOf(this['character'][0])]);
+                    }
+                    var content='<div id="main" class="main" style="background-image:url(images/forest.jpg)">';
                         //alert("Niythhin");
-			content=content+'<div id="place1" class="place1" style="background-image:url(images/'+this['character1']+')">'+this['description']+
+			content=content+'<div id="place1" class="place1" style="background-image:url(images/'+char1+')">'+this['discription']+
                                 '<div id="message1" class="message"><br>'
-                                +this['dialogue1']+
+                                +dia1+
                                 '</div></div>'+
-                                '<div id="place2" class="place2" style="background-image:url(images/'+this['character2']+')" >'+
+                                '<div id="place2" class="place2" style="background-image:url(images/'+char2+')" >'+
                                 '<div id="message1 class="message2"><br><br>'+
-                                this['dialogue2']+'</div></div>';
+                                dia2+'</div></div>';
                         bg=this['background'];
                         ch1=this['character1'];
                         ch2=this['character2'];
